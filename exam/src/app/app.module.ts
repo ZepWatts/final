db75d2ef9862cf5c9d8aba34da6b69d724ee951a
Zep Watts <zepwatts@gmail.com>
2019-09-13 19:02:14 +0400

    Final exam

--- exam/src/app/app.module.ts
+++ exam/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { WorksComponent } from './pages/works/works.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HireUsComponent } from './pages/hire-us/hire-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WorksComponent,
+    AboutComponent,
+    ContactComponent,
+    HireUsComponent
   ],
   imports: [
     BrowserModule,
